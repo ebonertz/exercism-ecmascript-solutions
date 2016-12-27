dda2a249da6537f1d9e3dedf74ccc8d0bc9b1c05
Evan Bonertz <evan.bonertz@commercetools.com>
2016-12-27 16:40:41 -0500

    All specs pass

--- bob/babel-output/bob.js
+++ bob/babel-output/bob.js
@@ -16,9 +16,12 @@ var Bob = (function () {
   _createClass(Bob, [{
     key: 'hey',
     value: function hey(message) {
-      if (message.indexOf('!') > 0) return 'Whoa, chill out!';
-      if (message.indexOf('?') > 0) return 'Sure.';
-
+      this.message = message;
+      if (message.endsWith('?') && message === message.toUpperCase() && message.match(/[a-z]/i)) return 'Whoa, chill out!';
+      if (message.endsWith('!') && message === message.toUpperCase() && message.match(/[a-z]/i)) return 'Whoa, chill out!';
+      if (message === message.toUpperCase() && message.match(/[a-z]/i)) return 'Whoa, chill out!';
+      if (message.endsWith('?')) return 'Sure.';
+      if (message.trim().length === 0) return 'Fine. Be that way!';
       return 'Whatever.';
     }
   }]);
--- bob/bob.js
+++ bob/bob.js
@@ -1,8 +1,11 @@
 class Bob {
   hey(message) {
-    if (message.indexOf('!')>0) return 'Whoa, chill out!';
-    if (message.indexOf('?')>0) return 'Sure.';
-
+    this.message = message;
+    if (message.endsWith('?') && message === message.toUpperCase() && message.match(/[a-z]/i)) return 'Whoa, chill out!';
+    if (message.endsWith('!') && message === message.toUpperCase() && message.match(/[a-z]/i)) return 'Whoa, chill out!';
+    if (message === message.toUpperCase() && message.match(/[a-z]/i)) return 'Whoa, chill out!';
+    if (message.endsWith('?')) return 'Sure.';
+    if (message.trim().length === 0) return 'Fine. Be that way!';
     return 'Whatever.';
   }
 }
